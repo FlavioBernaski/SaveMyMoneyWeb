744362571cbeca48112f215a59b9ba0e49b7e346
flavio <fbernaski@gmail.com>
2023-12-04 20:52:58 -0300

    Alterado authHeader para getHeader pois ele não estava dando update no estado

--- src/hooks/useApi.ts
+++ src/hooks/useApi.ts
@@ -11,12 +11,14 @@ const api = axios.create({
 api.defaults.headers.get['Access-Control-Allow-Origin'] = '*';
 api.defaults.headers.get['Content-Type'] = 'application/json;charset=utf-8';
 api.defaults.headers.get['Accept'] = 'application/json;charset=utf-8';
-const token = localStorage.getItem('token');
 
-const authHeader = {
-    headers: {
-        'Authorization': `Bearer ${token}`
-    }
+const getHeader = () => {
+    return ({
+            headers: {
+                'Authorization': 'Bearer ' + localStorage.getItem('token')
+            }
+        }
+    )
 }
 
 export const useApi = () => useMemo(() => ({
@@ -35,49 +37,49 @@ export const useApi = () => useMemo(() => ({
     },
     // Movimentações
     listarMovimentacoes: async () => {
-        const response = await api.get("/movimentacoes", authHeader);
+        const response = await api.get("/movimentacoes", getHeader());
         return response.data;
     },
     cadastrarMovimentacao: async (movimentacao: Movimentacao) => {
-        const response = await api.post('/movimentacoes', movimentacao, authHeader);
+        const response = await api.post('/movimentacoes', movimentacao, getHeader());
         return response.data;
     },
     // Cartões
     listarCartoes: async () => {
-        const response = await api.get("/cartoes", authHeader);
+        const response = await api.get("/cartoes", getHeader());
         return response.data;
     },
     listarCartoesPorConta: async (idConta: string) => {
-        const response = await api.get("/cartoes/conta/" + idConta, authHeader);
+        const response = await api.get("/cartoes/conta/" + idConta, getHeader());
         return response.data;
     },
     excluirCartao: async (id: string) => {
-        const response = await api.delete("/cartoes/" + id, authHeader);
+        const response = await api.delete("/cartoes/" + id, getHeader());
         return response.data;
     },
     cadastrarCartao: async (cartao: Cartao) => {
-        const response = await api.post("/cartoes", cartao, authHeader);
+        const response = await api.post("/cartoes", cartao, getHeader());
         return response.data;
     },
     localizarCartao: async (idCartao: string) => {
-        const response = await api.get('/cartoes/' + idCartao, authHeader);
+        const response = await api.get('/cartoes/' + idCartao, getHeader());
         return response.data;
     },
     // Contas
     listarContas: async () => {
-        const response = await api.get('/contas', authHeader);
+        const response = await api.get('/contas', getHeader());
         return response.data;
     },
     cadastrarConta: async (conta: Conta) => {
-        const response = await api.post('/contas', conta, authHeader);
+        const response = await api.post('/contas', conta, getHeader());
         return response.data;
     },
     localizarConta: async (idConta: string) => {
-        const response = await api.get('/contas/' + idConta, authHeader);
+        const response = await api.get('/contas/' + idConta, getHeader());
         return response.data;
     },
     excluirConta: async (id: string) => {
-        const response = await api.delete("/contas/" + id, authHeader);
+        const response = await api.delete("/contas/" + id, getHeader());
         return response.data;
     }
 }), []); // Fecha useMemo
